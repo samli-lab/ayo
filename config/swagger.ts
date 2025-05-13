3e7461929ffdd4d0e22691acbd076290675a1f23
Sam Li <samdleeone@gmail.com>
2025-05-13 14:24:42 +0800

    feat: add swagger

---
+++ config/swagger.ts
@@ -0,0 +1,32 @@
+// for AdonisJS v6
+import path from 'node:path'
+import url from 'node:url'
+// ---
+
+export default {
+  path: path.dirname(url.fileURLToPath(import.meta.url)) + '/../doc/swagger/', // 修改输出路径到 doc/swagger
+  title: 'Ayo API Document', // use info instead
+  version: '1.0.0', // use info instead
+  description: '', // use info instead
+  tagIndex: 2,
+  productionEnv: 'production', // optional
+  info: {
+    title: 'Ayo API Doc',
+    version: '1.0.0',
+    description: '',
+  },
+  snakeCase: true,
+
+  debug: false, // set to true, to get some useful debug output
+  ignore: ['/swagger', '/docs'],
+  preferredPutPatch: 'PUT', // if PUT/PATCH are provided for the same route, prefer PUT
+  common: {
+    parameters: {}, // OpenAPI conform parameters that are commonly used
+    headers: {}, // OpenAPI conform headers that are commonly used
+  },
+  securitySchemes: {}, // optional
+  authMiddlewares: ['auth', 'auth:api'], // optional
+  defaultSecurityScheme: 'BearerAuth', // optional
+  persistAuthorization: true, // persist authorization between reloads on the swagger page
+  showFullPath: false, // the path displayed after endpoint summary
+}
--- start/routes.ts
+++ start/routes.ts
@@ -6,10 +6,12 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
 
 // 导入所有路由模块
 import '#routes/user'
-import '#routes/comment'
+// import '#routes/comment'
 
 // 默认路由
 import router from '@adonisjs/core/services/router'
@@ -19,4 +21,16 @@ router.get('/', async () => {
     message: 'Welcome to API',
     version: '1.0.0',
   }
+})
+
+// returns swagger in YAML
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
+  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead. If you want, you can pass proxy url as second argument here.
+  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
 })
