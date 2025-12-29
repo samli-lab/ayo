4b831371dc2fe2920638ba9b89ce7fecd4789e1b
Sam Li <samdleeone@gmail.com>
2025-12-29 21:41:49 +0800

    feat: add authentication to blog/gallery write routes and date-based log naming

--- app/routes/gallery.ts
+++ app/routes/gallery.ts
@@ -1,5 +1,6 @@
 import router from '@adonisjs/core/services/router'
 import { apiThrottle } from '#start/limiter'
+import { middleware } from '#start/kernel'
 
 const GalleryController = () => import('#controllers/blog/gallery_controller')
 
@@ -12,12 +13,18 @@ router
     router.get('/gallery/:id', [GalleryController, 'show']).use(apiThrottle)
 
     // 添加照片
-    router.post('/gallery', [GalleryController, 'store']).use(apiThrottle)
+    router.post('/gallery', [GalleryController, 'store']).use(middleware.auth()).use(apiThrottle)
 
     // 更新照片
-    router.put('/gallery/:id', [GalleryController, 'update']).use(apiThrottle)
+    router
+      .put('/gallery/:id', [GalleryController, 'update'])
+      .use(middleware.auth())
+      .use(apiThrottle)
 
     // 删除照片
-    router.delete('/gallery/:id', [GalleryController, 'destroy']).use(apiThrottle)
+    router
+      .delete('/gallery/:id', [GalleryController, 'destroy'])
+      .use(middleware.auth())
+      .use(apiThrottle)
   })
   .prefix('/api')
